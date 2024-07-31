a5b111f8eb59429a8f4925f776c4e26c145115b9
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2024-07-31 17:49:40 +0900

    fix: refer model url

--- apgcc/models/backbones/vgg.py
+++ apgcc/models/backbones/vgg.py
@@ -94,8 +94,7 @@ def _vgg(arch, cfg, batch_norm, pretrained, progress, sync=False, **kwargs):
         kwargs['init_weights'] = False
     model = VGG(make_layers(cfgs[cfg], batch_norm=batch_norm, sync=sync), **kwargs)
     if pretrained:
-        state_dict = torch.load(model_paths[arch])
-        model.load_state_dict(state_dict)
+        model.load_state_dict(torch.hub.load_state_dict_from_url(model_urls[arch]))
     return model
 
 def vgg11(pretrained=False, progress=True, **kwargs):
